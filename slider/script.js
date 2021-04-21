66f98879d7d1a7b4e6e6a6c66fcb7e87e070bd46
NaanAvelinoCardoso <naanavelino2018@gmail.com>
2021-04-21 15:12:29 -0300

    atualizando codigo

--- slider/script.js
+++ slider/script.js
@@ -1,28 +1,34 @@
+// variaveis de ambientes
 let totalSlides = document.querySelectorAll('.slide-item').length;
 let currentSlide = 0;
 
+//setando o width dos slides e height do controls
 document.querySelector('.slide-width').style.width = `calc(100vw * ${totalSlides})`;
 document.querySelector('.slide-controls').style.height = `${document.querySelector('.slide').clientHeight}px`;
 
+// criando as funções de passa o slide
 function goPrev() {
     currentSlide--;
     if (currentSlide < 0) {
-        currentSlide = totalSlides - 1;
+        currentSlide = totalSlides -1;
     }
-    updateMargin();
+
+    updateSlide();
 }
 function goNext() {
     currentSlide++;
-    if (currentSlide > (totalSlides-1)) {
+    if (currentSlide > (totalSlides - 1)) {
         currentSlide = 0;
     }
 
-    updateMargin();
+    updateSlide();
 }
-function updateMargin() {
+// função que faz os controles funcionar
+function updateSlide() {
     let slideItemWidth = document.querySelector('.slide-item').clientWidth;
     let newMargin = (currentSlide * slideItemWidth);
     document.querySelector('.slide-width').style.marginLeft = `-${newMargin}px`;
 }
 
+// criando esse intervalo de cada 5s ele ficar trocando o slide
 setInterval(goNext, 5000);
